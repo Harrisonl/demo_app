5a6a9823143c12e73f3dc195732682bf3b03002a
Nicolas Dabreo <nicolas@megalixir.co.uk>
2024-02-20 16:30:51 +0000

    Read and display pubsub usage data

--- lib/demo_app_web/live/report_live.ex
+++ lib/demo_app_web/live/report_live.ex
@@ -11,13 +11,32 @@ defmodule DemoAppWeb.ReportLive do
       }
     ]
 
-    {:ok, socket
-      |> assign(:users, users)}
+    {:ok,
+    socket
+    |> assign(:users, users)}
   end
 
   def handle_params(params, uri, socket) do
-    {:noreply, socket}
+    {:noreply,
+      socket
+      |> apply_action(socket.assigns.live_action, params)}
   end
 
+  defp apply_action(socket, :index, params) do
+    socket
+  end
 
+  defp apply_action(socket, :user, %{"user_id" => user_id}) do
+    %{username: username} = Enum.find(socket.assigns.users, &(&1.id == user_id))
+    UsagePubSub.subscribe(username)
+
+    socket
+    |> stream(:usage, [])
+  end
+
+  defp handle_info({:current_usage, payload}, socket) do
+    {:noreply,
+      socket
+      |> stream_insert(:usage, payload)}
+  end
 end
